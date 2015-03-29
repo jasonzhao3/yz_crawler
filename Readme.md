39198e90e1c57444dcc1f09fa6c06763d20b4bc4
jason zhao <yzhao3@stanford.edu>
2015-03-29 14:43:57 -0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Simple Web Crawler
+====================
+
+This is a simple web crawler, forked from Alan Skorkin's post at http://www.skorks.com/2009/07/how-to-write-a-web-crawler-in-ruby/
+
+It is aimed to be a good practice of writing a simple ruby application and building a small gem.
+
+
+References
+===========
+http://guides.rubygems.org/name-your-gem/
+http://guides.rubygems.org/patterns/#consistent-naming
+http://guides.rubygems.org/make-your-own-gem/
---
+++ search-engine-main.rb
@@ -0,0 +1,21 @@
+require './spider'
+require './command_line_argument_parser'
+require './url_store'
+
+argument_parser = CommandLineArgumentParser.new
+argument_parser.parse_arguments
+spider = Spider.new
+url_store = UrlStore.new(argument_parser.url_file)
+
+if argument_parser.crawl_type == CommandLineArgumentParser::WEB_CRAWLER
+	spider.crawl_web(
+		url_store.get_urls, 
+		argument_parser.crawl_depth, 
+    argument_parser.page_limit
+  ) 
+else
+  spider.crawl_domain(
+    url_store.get_url, 
+    argument_parser.page_limit
+  )
+end
